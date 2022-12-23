9b4ed57612d5977d4674713e46a96acbe7b1dfe5
Katja Roehlig <katja-roehlig@mail.de>
2022-12-23 22:05:20 +0100

    feat: add functionality

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
+<script>
+import q from "./assets/data/data.json";
+import { ref, watch } from "vue";
+import CardComponent from "./components/CardComponent.vue";
+
+export default {
+  components: { CardComponent },
+  setup() {
+    const quizes = ref(q);
+    const search = ref("");
+
+    watch(search, () => {
+      quizes.value = q.filter((element) =>
+        element.name.toLowerCase().includes(search.value.toLowerCase())
+      );
+    });
+    return { quizes, search, CardComponent };
+  },
+};
+</script>
+
 <template>
   <div class="main__container">
     <header class="header">
       <h1 class="header__heading">Quiz App</h1>
-      <input class="header__input" type="text" placeholder="Search" />
+      <input
+        v-model.trim="search"
+        class="header__input"
+        type="text"
+        placeholder="Search"
+      />
+      <p>{{ search }}</p>
     </header>
     <div class="card__container">
-      <div class="card">
-        <img
-          class="card__image"
-          src="https://picsum.photos/220"
-          alt="ein Foto"
-        />
-        <section class="card__text">
-          <h2 class="card__heading">Math</h2>
-          <p class="card__questions">15 questions</p>
-        </section>
-      </div>
+      <CardComponent v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -26,6 +43,7 @@
   max-width: 90vw;
   margin: 0 auto;
 }
+
 .header {
   border-bottom: 2px solid hotpink;
   padding: 3rem 1rem 2rem 1rem;
@@ -51,41 +69,9 @@
   outline: 2px solid hotpink;
   appearance: none;
 }
-/* card-style ***************************************************************/
 .card__container {
   margin-top: 2.5rem;
   display: flex;
   flex-wrap: wrap;
-}
-
-.card {
-  border: 1px solid hotpink;
-  border-radius: 0.4rem;
-  padding: 0.7rem;
-  overflow: hidden;
-  box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.6);
-  margin-bottom: 2rem;
-  margin-right: 2rem;
-  cursor: pointer;
-}
-.card__image {
-  overflow: hidden;
-  border-radius: 0.3rem;
-}
-.card__text {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-top: 0.4rem;
-}
-.card__heading {
-  font-weight: bold;
-  color: hsl(330, 100%, 51%);
-  padding: 0 3rem 0rem 3rem;
-  border-bottom: 1px solid hotpink;
-}
-.card__questions {
-  padding-top: 1.5rem;
 }
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  props: ["quiz"],
+  setup() {},
+};
+</script>
+
+<template>
+  <div class="card">
+    <img class="card__image" :src="quiz.img" alt="ein Foto" />
+    <section class="card__text">
+      <h2 class="card__heading">{{ quiz.name }}</h2>
+      <p class="card__questions">{{ quiz.questions.length }} questions</p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid hotpink;
+  border-radius: 0.4rem;
+  padding: 0.7rem;
+  overflow: hidden;
+  box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.6);
+  margin-bottom: 2rem;
+  margin-right: 2rem;
+  cursor: pointer;
+}
+.card__image {
+  overflow: hidden;
+  border-radius: 0.3rem;
+  width: 15rem;
+  height: 10rem;
+}
+.card__text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 0.4rem;
+}
+.card__heading {
+  font-weight: bold;
+  color: hsl(330, 100%, 51%);
+  padding: 0 3rem 0rem 3rem;
+  border-bottom: 1px solid hotpink;
+}
+.card__questions {
+  padding-top: 1.5rem;
+}
+</style>
